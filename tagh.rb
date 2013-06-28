480f04fef23b707e808da2de3f6b57edf0ba2402
Martin Saxer <jmsaxer@gmail.com>
2013-06-28 18:03:50 +0400

    added options -f and -s to command "list"

--- tagh.rb
+++ tagh.rb
@@ -14,7 +14,7 @@ end
 
 class Tagger
 
-	def list(source)
+	def list(source, options)
 
 		tags = []
 		source = '/Users/sx/Dropbox/Notes/'
@@ -26,16 +26,28 @@ class Tagger
 				f = File.open(p)
 				tags << f.read.scan(/( #[\w\d-]+)(?=\s|$)/i)	
 			
-		end
+		end		
 
 		tags = tags.delete_if(&:empty?).flatten.map(&:lstrip).sort.uniq
-		
 
-			# if options[:sublime]
-			# 	sublime = '{"scope": "text","completions":[' + tags.map { |e| '"' + e.strip + '"'}.join(",") + ']}'
-			# 	fpath = ENV['HOME'] + '/Library/Application Support/Sublime Text 2/Packages/User/tags.sublime-completions'
-			# 	File.open(fpath , 'w') { |file| file.puts sublime }
-			# end
+
+		if options[:sublime]
+			#create/update JSON file for Sublime Text autocompletion
+			sublime = '{"scope": "text","completions":[' + tags.map { |e| '"' + e.strip + '"'}.join(",") + ']}'
+			fpath = ENV['HOME'] + '/Library/Application Support/Sublime Text 2/Packages/User/tags.sublime-completions'
+			File.open(fpath , 'w') { |file| file.puts sublime }
+			puts "Sublime Text autocompletion list updated"
+		end
+
+		if options[:file]
+			File.open(options[:file], 'w') { |file| file.puts tags}
+			puts "List of tags writen to: " + options[:file]
+		else
+			#standard output
+			tags
+		end
+
+
 	end
 
 end
@@ -44,9 +56,10 @@ end
 class Tagh < Thor
 	desc "list source", "list all tags in 'source' (directory)"
 	option :sublime, :aliases => "-s"
+	option :file, :aliases => "-f"
 	def list(source)
 		r = Tagger.new
-   		puts r.list(source)	
+   		puts r.list(source, options)	
 	end
 end
 
